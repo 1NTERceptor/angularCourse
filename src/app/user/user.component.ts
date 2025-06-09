8b6771171019da3384a6e8460f8d59faeea91140
Janek <gornada@gmail.com>
2025-06-09 21:19:46 +0200

    Dodanie eventów i signala

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,5 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  //get imagePath() {
+  //  return `assets/users/` + this.selectedUser().avatar;
+  //}
+
+  onSelectUser() {
+    console.log('click!');
+
+    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+  }
 }
